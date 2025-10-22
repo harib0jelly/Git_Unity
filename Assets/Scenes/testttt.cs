bf38f8e82759a08829284a8861301d5d0fafdd6c
haribo <free789852@naver.com>
2025-10-22 10:53:03 +0900

    스크립트업뎃

--- Assets/Scenes/testttt.cs
+++ Assets/Scenes/testttt.cs
@@ -7,7 +7,7 @@ public class testttt : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("HELLOOOOO");
     }
 
     // Update is called once per frame
