a4aa4772aad76492ff5b55f1175e10a36d20bae8
haribo <free789852@naver.com>
2025-10-22 13:10:44 +0900

    Update PlayerController.cs

--- Assets/Scenes/PlayerController.cs
+++ Assets/Scenes/PlayerController.cs
@@ -6,7 +6,6 @@ public class PlayerController : MonoBehaviour
 {
     Vector3 move = Vector3.zero;
     Rigidbody2D myRigid;
-    Animator myAnim;
     SpriteRenderer mySr;
 
     bool isJump = false;
@@ -18,7 +17,6 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         myRigid = GetComponent<Rigidbody2D>();
-        myAnim = GetComponent<Animator>();
         mySr = GetComponent<SpriteRenderer>();
     }
 
@@ -30,24 +28,20 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 move = Vector3.right;
-                myAnim.SetBool("run", true);
             }
             else if (Input.GetKey(KeyCode.LeftArrow))
             {
                 move = Vector3.left;
-                myAnim.SetBool("run", true);
             }
             else
             {
                 move = Vector3.zero;
-                myAnim.SetBool("run", false);
             }
 
             if(Input.GetKeyDown(KeyCode.Space)&& isJump == false)
             {
                 move = Vector3.up;
                 myRigid.AddForce(Vector3.up * 300.0f);
-                myAnim.SetBool("jump", true);
                 isJump = true;
             }
             transform.Translate(move * speed * Time.deltaTime);
